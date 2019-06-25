ac0bb40aaaff949c029727f7a4ee0e2fa970e59e
haesleinhuepf <rhaase@mpi-cbg.de>
2019-06-25 11:21:32 +0200

    added Gaussian blur benchmarking IJ1/IJ2/CLIJ demo

---
+++ src/main/java/net/haesleinhuepf/clij/examples/GaussianBlurBenchmarkingDemo.java
@@ -0,0 +1,96 @@
+package net.haesleinhuepf.clij.examples;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.Prefs;
+import ij.plugin.Duplicator;
+import ij.plugin.ImageCalculator;
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.imagej.ImageJ;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.neighborhood.DiamondShape;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.roi.Regions;
+import net.imglib2.type.BooleanType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.util.Pair;
+import net.imglib2.view.Views;
+
+import java.io.IOException;
+
+/**
+ * GaussianBlurBenchmarkingDemo
+ * <p>
+ * <p>
+ * <p>
+ * Author: @haesleinhuepf
+ * 06 2019
+ */
+public class GaussianBlurBenchmarkingDemo {
+
+    private static ImageJ ij;
+    private static CLIJ clij;
+    private static double sigma = 3;
+
+    public static void main(String... args) throws IOException {
+
+        // ---------------------------------------
+        // Initialize ImageJ, CLIJ and test image
+        ij = new ImageJ();
+        ij.ui().showUI();
+        clij = CLIJ.getInstance();
+
+        input = IJ.openImage("https://imagej.net/images/flybrain.zip");
+        input.show();
+        IJ.run(input, "8-bit", "");
+        img = ImageJFunctions.wrapReal(input);
+
+        for (int i = 0; i < 100; i++) {
+            // ---------------------------------------
+            // three test scenarios
+            long timestamp = System.currentTimeMillis();
+            demoImageJ1();
+            System.out.println("The ImageJ1 way took " + (System.currentTimeMillis() - timestamp) + " msec");
+
+            timestamp = System.currentTimeMillis();
+            demoImageJ2();
+            System.out.println("The ImageJ2 way took " + (System.currentTimeMillis() - timestamp) + " msec");
+
+            timestamp = System.currentTimeMillis();
+            demoClearCLIJ();
+            System.out.println("The ClearCL way took " + (System.currentTimeMillis() - timestamp) + " msec");
+            // ---------------------------------------
+            System.out.println("-----");
+        }
+    }
+
+    private static ImagePlus input;
+    private static Img img;
+
+    private static void demoImageJ1() {
+        ImagePlus copy = new Duplicator().run(input, 1, input.getNSlices());
+        IJ.run(copy, "Gaussian Blur 3D...", "x=" + sigma + " y=" + sigma + " z=" + sigma + "");
+        copy.show();
+    }
+
+    private static <T extends RealType<T>, B extends BooleanType<B>> void demoImageJ2() {
+        RandomAccessibleInterval gauss = ij.op().filter().gauss(img, sigma);
+        ImageJFunctions.show(gauss);
+    }
+
+    private static void demoClearCLIJ() throws IOException {
+        ClearCLBuffer input = clij.push(img);
+        ClearCLBuffer blurred = clij.create(input);
+
+        clij.op().blur(input, blurred, (float) sigma, (float) sigma, (float) sigma);
+
+        clij.pull(blurred).show();
+
+        blurred.close();
+        input.close();
+    }
+}
